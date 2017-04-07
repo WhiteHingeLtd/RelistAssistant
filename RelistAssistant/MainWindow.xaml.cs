042d08d71849a46e134dc55a4e63823f3bde2bdf
OwenH <OwenH@whitehinge.com>
2017-04-07 13:22:48 +0100

    Initial

--- RelistAssistant/MainWindow.xaml.cs
+++ RelistAssistant/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WHLClasses;
 
 namespace RelistAssistant
 {
@@ -20,9 +24,67 @@ namespace RelistAssistant
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SkuCollection MainColl = new SkuCollection();
         public MainWindow()
         {
             InitializeComponent();
+            GenericDataController Loader = new GenericDataController();
+            MainColl = Loader.SmartSkuCollLoad(true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Misc.OperationDialog("Generating CSV", GenerateCsv); 
+        }
+
+        private void GenerateCsv(object sender, DoWorkEventArgs e)
+        {
+            ConcurrentBag<WhlSKU> DataBag = new ConcurrentBag<WhlSKU>();
+            List<WhlSKU> Datalist = new List<WhlSKU>();
+            Stopwatch newwatch = new Stopwatch();
+            newwatch.Reset();
+            newwatch.Start();
+            Parallel.ForEach(MainColl, sku =>
+            {
+                if (sku.PackSize == 1)
+                {
+                    if (sku.SKU.Contains("xxxx")) return;
+                    if (sku.Stock.Minimum < 3) return;
+                    if (sku.Stock.Level != 0) return;
+                    DataBag.Add(sku);
+
+                }
+                else
+                {
+                    if (!(sku.Stock.Minimum > sku.Stock.Level)) return;
+                    if (sku.SKU.Contains("xxxx")) return;
+                    DataBag.Add(sku);
+                }
+            }
+            );
+            Console.WriteLine(newwatch.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Finished Parallel");
+            Console.WriteLine(DataBag.Count);
+            Datalist.AddRange(DataBag);
+            Datalist.Sort((x,y) => x.GetLocation(SKULocation.SKULocationType.Pickable).LocationID.CompareTo(y.GetLocation(SKULocation.SKULocationType.Pickable).LocationID));
+            var csvbuilder = "Sku,Location,Title,Level,Minimum,Stock,Check" + Environment.NewLine;
+            foreach (WhlSKU sku in Datalist)
+            {
+                csvbuilder += sku.ShortSku + "," + sku.GetLocation(SKULocation.SKULocationType.Pickable).LocationText +","+sku.Title.Label + "," + sku.Stock.Level.ToString() + "," + sku.Stock.Minimum.ToString() + "," + sku.Stock.Total.ToString() + "," + Environment.NewLine;
+            }
+            try
+            {
+               File.WriteAllText(@"X:\Relisting\NewList.csv", csvbuilder);
+            }
+            catch (IOException exception)
+            {
+                File.WriteAllText(@"X:\Relisting\NewList1.csv", csvbuilder);
+                Console.WriteLine(exception);
+            }
+            newwatch.Stop();
+            Console.WriteLine(newwatch.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Finished");
+            //Console.Write(csvbuilder);
         }
     }
 }
